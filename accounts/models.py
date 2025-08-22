bb8ab366a1bcd03ce68880636cc467c7bbba01e1
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-08-22 11:53:07 +0300

    Implement custom User model and UserManager for authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,21 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from .managers import UserManager
+
 
 class User(AbstractUser):
-    # Ролі користувачів
     class Roles(models.TextChoices):
         ADMIN = "admin", "Адміністратор"
         WEIGHER = "weigher", "Вагар"
-
+        
     role = models.CharField(
         max_length=20,
         choices=Roles.choices,
-        default=Roles.DRIVER
+        default=Roles.WEIGHER
     )
+
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    objects = UserManager()
 
     def __str__(self):
         return f"{self.username} ({self.get_role_display()})"

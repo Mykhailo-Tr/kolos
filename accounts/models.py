849c013085b2801be371d8e07490a036a38af4d5
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-08-22 11:44:11 +0300

    Add User model with roles for authentication

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    # Ролі користувачів
+    class Roles(models.TextChoices):
+        ADMIN = "admin", "Адміністратор"
+        WEIGHER = "weigher", "Вагар"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.DRIVER
+    )
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"

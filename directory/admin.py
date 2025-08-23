35afd6b2a6d896efdff23152caf327fb9ab11e8c
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-08-23 23:19:09 +0300

    Add trailer, unloading place, and partner admin configurations: register models with display and search fields in the admin interface.

--- directory/admin.py
+++ directory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Driver, Car, Culture
+from .models import Driver, Car, Culture, Trailer, UnloadingPlace, Partner
 
 
 @admin.register(Driver)
@@ -18,3 +18,21 @@ class CarAdmin(admin.ModelAdmin):
 class CultureAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
+
+
+@admin.register(Trailer)
+class TrailerAdmin(admin.ModelAdmin):
+    list_display = ("number", "comment")
+    search_fields = ("number", "comment")
+
+
+@admin.register(UnloadingPlace)
+class UnloadingPlaceAdmin(admin.ModelAdmin):
+    list_display = ("name", "address", "place_type")
+    search_fields = ("name", "address", "place_type")
+
+
+@admin.register(Partner)
+class PartnerAdmin(admin.ModelAdmin):
+    list_display = ("name", "phone", "email")
+    search_fields = ("name", "phone", "email")

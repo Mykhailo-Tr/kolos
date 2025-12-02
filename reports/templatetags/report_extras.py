619ae27d7cd2b6b778521b6142366b44dd63ee26
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2025-12-02 22:32:32 +0200

    Remove stock balance and weigher journal report templates, associated views, utils, URL patterns, and custom template tags. Clean up tests and unused code related to reports.

--- reports/templatetags/report_extras.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def pluck(queryset, key):
-    """Витягує список значень по ключу з queryset/list of dicts"""
-    return [item.get(key) for item in queryset if item.get(key) is not None]

f2eb552b4578ae8ac8568e5d5a2ebe9607d7c2b3
Mox1toGH <denisbaluk187@gmail.com>
2025-12-20 21:07:08 +0200

    Add ActivityLogMiddleware to track user actions and log activity. NOT THE FINAL VERSION

--- activity/admin.py
+++ activity/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import ActivityLog
 
-# Register your models here.
+@admin.register(ActivityLog)
+class ActivityLogAdmin(admin.ModelAdmin):
+    # 1. Відображення всіх полів у списку
+    list_display = ('id', 'created_at', 'user', 'action', 'description_full')
+    
+    # 2. Додаємо можливість клікнути на користувача або дату для переходу в деталі
+    list_display_links = ('id', 'created_at')
+    
+    # 3. Фільтрація (справа)
+    list_filter = ('action', 'created_at', 'user')
+    
+    # 4. Пошук по всіх ключових полях
+    search_fields = ('user__username', 'description', 'action')
+    
+    # 5. Робимо поля доступними тільки для читання (щоб не можна було змінити історію)
+    readonly_fields = ('user', 'action', 'description', 'created_at')
+
+    # 6. Сортування (найновіші зверху)
+    ordering = ('-created_at',)
+
+    def description_full(self, obj):
+        """Відображає повний опис, щоб він не обрізався в таблиці"""
+        return obj.description
+    description_full.short_description = "Деталі дії"
+
+    # Щоб бачити більше деталей при натисканні на запис:
+    fieldsets = (
+        ('Основна інформація', {
+            'fields': ('created_at', 'user', 'action')
+        }),
+        ('Контент', {
+            'fields': ('description',),
+        }),
+    )
+
+    # Заборона редагування
+    def has_add_permission(self, request):
+        return False
--- kolos/settings.py
+++ kolos/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'activity.middleware.ActivityLogMiddleware',
 ]
 
 ROOT_URLCONF = 'kolos.urls'

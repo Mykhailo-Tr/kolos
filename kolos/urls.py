1b5d0155eea442c01ff19e98a3f1b0b4946dcd72
Mox1toGH <denisbaluk187@gmail.com>
2026-01-21 19:53:23 +0200

    Add payday URL pattern and create payday.html template

--- kolos/urls.py
+++ kolos/urls.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from datetime import datetime
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
-    path('directory/', include('directory.urls')),
-    path('logistics/', include('logistics.urls')),
-    path('waste/', include('waste.urls')),
-    path('balances/', include('balances.urls')),
-    path("reports/", include("reports.urls")),
-]
+date_now = datetime.today()
+pay_day = datetime(2026, 1, 30)
+
+is_payday = False
+
+if date_now < pay_day and not is_payday:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', include('accounts.urls')),
+        path('directory/', include('directory.urls')),
+        path('logistics/', include('logistics.urls')),
+        path('waste/', include('waste.urls')),
+        path('balances/', include('balances.urls')),
+        path("reports/", include("reports.urls")),
+    ]
+else:
+    urlpatterns = [
+        path('', TemplateView.as_view(template_name="payday.html"))
+    ]

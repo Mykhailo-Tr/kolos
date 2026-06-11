88bd5848ef6eba916806c87cde87b5201a549232
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2026-06-11 11:27:06 +0300

    Update pay_day date to June 30, 2026 in URL configuration

--- kolos/urls.py
+++ kolos/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 from datetime import datetime
 
 date_now = datetime.today()
-pay_day = datetime(2026, 1, 30)
+pay_day = datetime(2026, 6, 30)
 
 is_payday = False
 
